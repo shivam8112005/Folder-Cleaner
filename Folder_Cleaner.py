322384de40945d2492c37a08896cf44532b972c5
shivam shukla <shivamshukla8112005@gmail.com>
2025-01-08 22:09:02 +0530

    completed!..  arranging and cleaning of the folder

--- Folder_Cleaner.py
+++ Folder_Cleaner.py
@@ -10,6 +10,27 @@ def clean_folder(folder_path):
         file_path=os.path.join(folder_path, filename)
         if(os.path.isfile(file_path)):
             print(file_path)
+            file_extension=filename.split('.')[-1]
+            if file_extension:
+                sub_folder=f"{file_extension.upper()} Files"
+                sub_folder_path=create_subfolder_if_needed(folder_path, sub_folder)
+                new_file_path=os.path.join(sub_folder_path,filename)
+                if not os.path.exists(new_file_path):
+                     shutil.move(file_path,sub_folder_path)
+                     print(f"Moved {filename} -> {sub_folder}")
+                else:
+                    print(f"New file path already exists {new_file_path}")
+
+               
+
+
+def create_subfolder_if_needed(folder_path, sub_folder):
+    sub_folder_path=os.path.join(folder_path,sub_folder)
+    if not os.path.exists(sub_folder_path):
+        os.makedirs(sub_folder_path)
+    return sub_folder_path
+
+
 
 
 
